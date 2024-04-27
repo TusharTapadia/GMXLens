cf3df1a82cef6781af603da4425fbb82978c1399
TusharTapadia <tushartapadia12@gmail.com>
2024-04-27 20:58:29 +0530

    Added getMarketInfo function

--- src/GMXLens.sol
+++ src/GMXLens.sol
@@ -2,9 +2,17 @@ pragma solidity 0.8.21;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol"
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IReader} from "./interfaces/IReader.sol";
+import {IDataStore} from "./interfaces/IDataStore.sol";
+import {IPriceFeed} from "./interfaces/IPriceFeed.sol";
+import {IOracle} from "./interfaces/IOracle.sol";
+
+import {Market,Keys,Price,Calc,Precision,MarketPoolValueInfo} from "./Lib.sol";
+
 contract GMXLens is UUPSUpgradeable, OwnableUpgradeable {
-    using Math for int256;
+    // using Math for int256;
 
     struct MarketDataState {
         address marketToken;
@@ -33,6 +41,75 @@ contract GMXLens is UUPSUpgradeable, OwnableUpgradeable {
         uint256 maxOpenInterestUsdShort; // 30 decimals
     }
 
-    
+    IReader private immutable reader;
+    address private immutable dataStore;
+    address private immutable oracle;
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    /// @param reader_ address of reader contract
+    /// @param dataStore_ address of dataStore contract
+    /// @param oracle_ address of oracle contract
+    constructor(IReader _reader, address _dataStore, address _oracle) {
+        reader = _reader;
+        dataStore = _dataStore;
+        oracle = _oracle;
+        _disableInitializers();
+    }
+
+     function initialize() external initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function getMarketData(
+        address marketID
+    ) external view returns (MarketDataState memory marketDataState) {
+        Market.Props memory marketProps = reader.getMarket(dataStore, marketID);
+
+        marketDataState.marketToken = marketProps.marketToken;
+        marketDataState.indexToken = marketProps.indexToken;
+        marketDataState.longToken = marketProps.longToken;
+        marketDataState.shortToken = marketProps.shortToken;
+
+        Price.MarketPrices memory marketPrices = Price.MarketPrices(
+            tokenPrice(marketProps.indexToken),
+            tokenPrice(marketProps.longToken),
+            tokenPrice(marketProps.shortToken)
+        );
+    }
+
+    function tokenPrice(address _token) external returns(Price.Props memory){
+        IPriceFeed priceFeed = IPriceFeed(IDataStore(dataStore).getAddress(Keys.priceFeedKey(_token)));
+
+        if (address(priceFeed) == address(0)) {
+            Price.Props memory primaryPrice = IOracle(oracle).primaryPrices(
+                _token
+            );
+            require(
+                primaryPrice.min != 0 && primaryPrice.max != 0,
+                "Not able to fetch latest price"
+            );
+            return primaryPrice;
+        }
+
+        uint256 multiplier = getPriceFeedMultiplier(_token);
+
+        (, int256 _tokenPrice, , , ) = priceFeed.latestRoundData();
+
+        uint256 price = Precision.mulDiv(
+            SafeCast.toUint256(_tokenPrice),
+            multiplier,
+            Precision.FLOAT_PRECISION
+        );
+        return Price.Props(price, price);
+    }
+
+   /**
+     * @dev performs required checks required to upgrade contract
+     * @param newImplementation address to update implementation logic to
+     */
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+    
 }
---
+++ src/Lib.sol
@@ -0,0 +1,295 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import "../node_modules/prb-math/contracts/PRBMathUD60x18.sol";
+
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/utils/math/SignedMath.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+library Price {
+    // @param marketToken address of the market token for the market
+    // @param indexToken address of the index token for the market
+    // @param longToken address of the long token for the market
+    // @param shortToken address of the short token for the market
+    // @param data for any additional data
+    struct Props {
+        uint256 min;
+        uint256 max;
+    }
+
+    struct MarketPrices {
+        Props indexTokenPrice;
+        Props longTokenPrice;
+        Props shortTokenPrice;
+    }
+}
+
+library MarketPoolValueInfo {
+    // @dev struct to avoid stack too deep errors for the getPoolValue call
+    // @param value the pool value
+    // @param longTokenAmount the amount of long token in the pool
+    // @param shortTokenAmount the amount of short token in the pool
+    // @param longTokenUsd the USD value of the long tokens in the pool
+    // @param shortTokenUsd the USD value of the short tokens in the pool
+    // @param totalBorrowingFees the total pending borrowing fees for the market
+    // @param borrowingFeePoolFactor the pool factor for borrowing fees
+    // @param impactPoolAmount the amount of tokens in the impact pool
+    // @param longPnl the pending pnl of long positions
+    // @param shortPnl the pending pnl of short positions
+    // @param netPnl the net pnl of long and short positions
+    struct Props {
+        int256 poolValue;
+        int256 longPnl;
+        int256 shortPnl;
+        int256 netPnl;
+        uint256 longTokenAmount;
+        uint256 shortTokenAmount;
+        uint256 longTokenUsd;
+        uint256 shortTokenUsd;
+        uint256 totalBorrowingFees;
+        uint256 borrowingFeePoolFactor;
+        uint256 impactPoolAmount;
+    }
+}
+
+library Market {
+    // @param marketToken address of the market token for the market
+    // @param indexToken address of the index token for the market
+    // @param longToken address of the long token for the market
+    // @param shortToken address of the short token for the market
+    // @param data for any additional data
+    struct Props {
+        address marketToken;
+        address indexToken;
+        address longToken;
+        address shortToken;
+    }
+
+    struct MarketInfo {
+        Market.Props market;
+        uint256 borrowingFactorPerSecondForLongs;
+        uint256 borrowingFactorPerSecondForShorts;
+        BaseFundingValues baseFunding;
+        GetNextFundingAmountPerSizeResult nextFunding;
+        VirtualInventory virtualInventory;
+        bool isDisabled;
+    }
+
+    struct BaseFundingValues {
+        PositionType fundingFeeAmountPerSize;
+        PositionType claimableFundingAmountPerSize;
+    }
+
+    struct PositionType {
+        CollateralType long;
+        CollateralType short;
+    }
+
+    struct CollateralType {
+        uint256 longToken;
+        uint256 shortToken;
+    }
+
+    struct GetNextFundingAmountPerSizeResult {
+        bool longsPayShorts;
+        uint256 fundingFactorPerSecond;
+        int256 nextSavedFundingFactorPerSecond;
+
+        PositionType fundingFeeAmountPerSizeDelta;
+        PositionType claimableFundingAmountPerSizeDelta;
+    }
+
+    struct VirtualInventory {
+        uint256 virtualPoolAmountForLongToken;
+        uint256 virtualPoolAmountForShortToken;
+        int256 virtualInventoryForPositions;
+    }
+}
+
+// there is a known issue with prb-math v3.x releases
+// https://github.com/PaulRBerg/prb-math/issues/178
+// due to this, either prb-math v2.x or v4.x versions should be used instead
+
+/**
+ * @title Precision
+ * @dev Library for precision values and conversions
+ */
+library Precision {
+    using SafeCast for uint256;
+    using SignedMath for int256;
+
+    uint256 public constant FLOAT_PRECISION = 10 ** 30;
+    uint256 public constant FLOAT_PRECISION_SQRT = 10 ** 15;
+
+    uint256 public constant WEI_PRECISION = 10 ** 18;
+    uint256 public constant BASIS_POINTS_DIVISOR = 10000;
+
+    uint256 public constant FLOAT_TO_WEI_DIVISOR = 10 ** 12;
+
+    /**
+     * Applies the given factor to the given value and returns the result.
+     *
+     * @param value The value to apply the factor to.
+     * @param factor The factor to apply.
+     * @return The result of applying the factor to the value.
+     */
+    function applyFactor(uint256 value, uint256 factor) internal pure returns (uint256) {
+        return mulDiv(value, factor, FLOAT_PRECISION);
+    }
+
+    /**
+     * Applies the given factor to the given value and returns the result.
+     *
+     * @param value The value to apply the factor to.
+     * @param factor The factor to apply.
+     * @return The result of applying the factor to the value.
+     */
+    function applyFactor(uint256 value, int256 factor) internal pure returns (int256) {
+        return mulDiv(value, factor, FLOAT_PRECISION);
+    }
+
+    function applyFactor(uint256 value, int256 factor, bool roundUpMagnitude) internal pure returns (int256) {
+        return mulDiv(value, factor, FLOAT_PRECISION, roundUpMagnitude);
+    }
+
+    function mulDiv(uint256 value, uint256 numerator, uint256 denominator) internal pure returns (uint256) {
+        return Math.mulDiv(value, numerator, denominator);
+    }
+
+    function mulDiv(int256 value, uint256 numerator, uint256 denominator) internal pure returns (int256) {
+        return mulDiv(numerator, value, denominator);
+    }
+
+    function mulDiv(uint256 value, int256 numerator, uint256 denominator) internal pure returns (int256) {
+        uint256 result = mulDiv(value, numerator.abs(), denominator);
+        return numerator > 0 ? result.toInt256() : -result.toInt256();
+    }
+
+    function mulDiv(uint256 value, int256 numerator, uint256 denominator, bool roundUpMagnitude) internal pure returns (int256) {
+        uint256 result = mulDiv(value, numerator.abs(), denominator, roundUpMagnitude);
+        return numerator > 0 ? result.toInt256() : -result.toInt256();
+    }
+
+    function mulDiv(uint256 value, uint256 numerator, uint256 denominator, bool roundUpMagnitude) internal pure returns (uint256) {
+        if (roundUpMagnitude) {
+            return Math.mulDiv(value, numerator, denominator, Math.Rounding.Ceil);
+        }
+
+        return Math.mulDiv(value, numerator, denominator);
+    }
+
+    function applyExponentFactor(
+        uint256 floatValue,
+        uint256 exponentFactor
+    ) internal pure returns (uint256) {
+        // `PRBMathUD60x18.pow` doesn't work for `x` less than one
+        if (floatValue < FLOAT_PRECISION) {
+            return 0;
+        }
+
+        if (exponentFactor == FLOAT_PRECISION) {
+            return floatValue;
+        }
+
+        // `PRBMathUD60x18.pow` accepts 2 fixed point numbers 60x18
+        // we need to convert float (30 decimals) to 60x18 (18 decimals) and then back to 30 decimals
+        uint256 weiValue = PRBMathUD60x18.pow(
+            floatToWei(floatValue),
+            floatToWei(exponentFactor)
+        );
+
+        return weiToFloat(weiValue);
+    }
+
+    function toFactor(uint256 value, uint256 divisor, bool roundUpMagnitude) internal pure returns (uint256) {
+        if (value == 0) { return 0; }
+
+        if (roundUpMagnitude) {
+            return Math.mulDiv(value, FLOAT_PRECISION, divisor, Math.Rounding.Ceil);
+        }
+
+        return Math.mulDiv(value, FLOAT_PRECISION, divisor);
+    }
+
+    function toFactor(uint256 value, uint256 divisor) internal pure returns (uint256) {
+        return toFactor(value, divisor, false);
+    }
+
+    function toFactor(int256 value, uint256 divisor) internal pure returns (int256) {
+        uint256 result = toFactor(value.abs(), divisor);
+        return value > 0 ? result.toInt256() : -result.toInt256();
+    }
+
+    /**
+     * Converts the given value from float to wei.
+     *
+     * @param value The value to convert.
+     * @return The converted value in wei.
+     */
+    function floatToWei(uint256 value) internal pure returns (uint256) {
+        return value / FLOAT_TO_WEI_DIVISOR;
+    }
+
+    /**
+     * Converts the given value from wei to float.
+     *
+     * @param value The value to convert.
+     * @return The converted value in float.
+     */
+    function weiToFloat(uint256 value) internal pure returns (uint256) {
+        return value * FLOAT_TO_WEI_DIVISOR;
+    }
+
+    /**
+     * Converts the given number of basis points to float.
+     *
+     * @param basisPoints The number of basis points to convert.
+     * @return The converted value in float.
+     */
+    function basisPointsToFloat(uint256 basisPoints) internal pure returns (uint256) {
+        return basisPoints * FLOAT_PRECISION / BASIS_POINTS_DIVISOR;
+    }
+}
+
+library Calc {
+    using SignedMath for int256;
+    using SafeCast for uint256;
+
+    // this method assumes that min is less than max
+    function boundMagnitude(int256 value, uint256 min, uint256 max) internal pure returns (int256) {
+        uint256 magnitude = value.abs();
+
+        if (magnitude < min) {
+            magnitude = min;
+        }
+
+        if (magnitude > max) {
+            magnitude = max;
+        }
+
+        int256 sign = value == 0 ? int256(1) : value / value.abs().toInt256();
+
+        return magnitude.toInt256() * sign;
+    }
+
+    /**
+     * @dev Calculates the absolute difference between two numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the absolute difference between the two numbers
+     */
+    function diff(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a - b : b - a;
+    }
+}
+
+library Keys {
+    bytes32 public constant PRICE_FEED = keccak256(abi.encode("PRICE_FEED"));
+
+
+    function priceFeedKey(address _token) internal pure returns (bytes32) {
+        return keccak256(abi.encode(PRICE_FEED, _token));
+    }
+}
